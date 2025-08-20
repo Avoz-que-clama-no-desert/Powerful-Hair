9b34de412947ef2cedecd22aa26491a16512c154
A voz que clama no deserto <devedu7957@gmail.com>
2025-08-20 10:06:15 -0300

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -33,9 +33,11 @@ var v3 = document.querySelector(".v3")
 var v4 = document.querySelector(".v4")
 var v5 = document.querySelector(".v5")
 var vtal = document.querySelector(".vtal")
-document.querySelector(".num").textContent="Documento"+" Nº  "+ Math.random()*1000
-document.querySelector(".dat").textContent="Criado pela primeira vez:"+dia+"/"+nemes[mes]+"/"+ano
-document.querySelector(".venci").textContent="Data de vencmento:"+dia+"/"+nemes[mes]+"/"+ano
+document.querySelector(".num").textContent= Math.random()*1000
+document.querySelector(".dat").textContent=dia+"/"+nemes[mes]+"/"+ano
+document.querySelector(".venci").textContent=dia+"/"+nemes[mes]+"/"+ano
+mes=mes+1
+document.querySelector(".utvz").textContent=1+"/"+nemes[mes]+"/"+ano
 
 
 var nomepro1 = document.querySelector(".nomepro1")
@@ -58,6 +60,11 @@ var nomepro5 = document.querySelector(".nomepro5")
 var pre5 = document.querySelector(".precopro5")
 var quant5 = document.querySelector(".unidpro5")
 
+var valqfalta = document.querySelector(".valqfalta")
+
+
+const vap = document.querySelector(".vafinal")
+
 
 
 
@@ -66,66 +73,129 @@ var quant5 = document.querySelector(".unidpro5")
 
 
 var cont = 0
-
+var soma = ""
 btn1.addEventListener("click",()=>{
-    const select = document.getElementById("selec");
+var unid = document.querySelector(".unidade").value
+const select = document.getElementById("selec");
   const valor = select.options[select.selectedIndex].value;
+  var confericliente = document.querySelector(".nome").value
+
+ if (confericliente=="") {
+  alert(" Primeiro coloque o nome do cliente")
+ } else {
+  if (valor=="") {
+  alert("É obrigado selecionar  produto")
+}else{
+if (unid=="") {
+  alert("caixa de unidade estar vazia")
+}else{
+/*******inicio de comando de conteudo*********** */
+if (cont==5) {
+  cont=0
+  alert("Só pode fazer 5 pedidos querendo cadastra para mais produtos fale com o programador")
   
- var unid = document.querySelector(".unidade").value
+} else {
+/*****aqui confere as 5 pedido*******/  
+
+document.querySelector(".client").textContent="-"+document.querySelector(".nome").value
  cont++
- var soma = ""
+ 
 // adicionar produtos
 if (cont==1) {
+ document.querySelector(".compra1").style.display="block" 
+
   nomepro1.textContent=produto[valor]
+
   quant1.textContent=unid
-   pre1.textContent=preco[valor]+",00"
-  v1.textContent=preco[valor]*unid+",00"
-   vtal.textContent=v1.textContent
-  
-}
-if (cont==2) {
-  nomepro2.textContent=produto[valor]
-  quant2.textContent=unid
-   pre2.textContent=preco[valor]+",00"
-  v2.textContent=preco[valor]*unid+",00"
-  
-  vtal.textContent=parseInt(v2.textContent)+parseInt(v1.textContent)+",00"
-  
-}
-if (cont==3) {
-  nomepro3.textContent=produto[valor]
-  quant3.textContent=unid
-   pre3.textContent=preco[valor]+",00"
-  v3.textContent=preco[valor]*unid+",00"
-  
-  vtal.textContent=parseInt(v2.textContent)+parseInt(v1.textContent)+parseInt(v3.textContent)+ ",00"
-  
-}
+
+   pre1.textContent="R$"+"   "+preco[valor]+",00"
+
+  v1.textContent= "R$"+"   "+preco[valor]*unid+",00"
  
-if (cont==4) {
-  nomepro4.textContent=produto[valor]
-  quant4.textContent=unid
-   pre4.textContent=preco[valor]+",00"
-  v4.textContent=preco[valor]*unid+",00"
+soma=+parseInt(preco[valor]*unid)
+
+vap.textContent="Soma dos serviços/itens    R$"+"     "+soma+",00"
+valqfalta.textContent=soma+".00"
   
-  vtal.textContent=parseInt(v2.textContent)+parseInt(v1.textContent)+parseInt(v3.textContent)+parseInt(v4.textContent) +",00"
+select.selectedIndex=""
+ document.querySelector(".unidade").value=""
   
-}
-if (cont==5) {
-  nomepro5.textContent=produto[valor]
-  quant5.textContent=unid
-   pre5.textContent=preco[valor]+",00"
-  v5.textContent=preco[valor]*unid+",00"
   
-  vtal.textContent=parseInt(v2.textContent)+parseInt(v1.textContent)+parseInt(v3.textContent)+parseInt(v4.textContent)+parseInt(v5.textContent)+",00"
-  cont=0
 }
 
-//*********************** */
-select.selectedIndex = "";
-document.querySelector(".unidade").value=""
- 
+if (cont==2) {
+    document.querySelector(".compra2").style.display="block"
+    document.querySelector(".compra2").style.display="block"
+  nomepro2.textContent=produto[valor]
+  quant2.textContent=unid
+   pre2.textContent="R$"+"   "+preco[valor]+",00"
+  v2.textContent="R$"+"   "+preco[valor]*unid+",00"
+  soma=soma+preco[valor]*unid
+  vap.textContent="Soma dos serviços/itens    R$"+"     "+soma+",00"
+  valqfalta.textContent=soma+".00"
+  select.selectedIndex=""
+  document.querySelector(".unidade").value=""
   
+}
+
+if (cont==3) {
+    document.querySelector(".compra3").style.display="block"
+  nomepro3.textContent=produto[valor]
+  quant3.textContent=unid
+   pre3.textContent="R$"+"   "+preco[valor]+",00"
+  v3.textContent="R$"+"   "+preco[valor]*unid+",00"
+  soma=soma+preco[valor]*unid
+  vap.textContent="Soma dos serviços/itens    R$"+"     "+soma+ ",00"
+  valqfalta.textContent=soma+".00"
+  select.selectedIndex=""
+  document.querySelector(".unidade").value=""
+
+}
+
+if (cont==4) {
+    document.querySelector(".compra4").style.display="block"
+  nomepro4.textContent=produto[valor]
+  quant4.textContent=unid
+   pre4.textContent="R$"+"   "+preco[valor]+",00"
+  v4.textContent="R$"+"   "+preco[valor]*unid+",00"
+  soma=soma+preco[valor]*unid
+  vap.textContent="Soma dos serviços/itens    R$"+"     "+soma+",00"
+  valqfalta.textContent=soma+".00"
+  select.selectedIndex=""
+  document.querySelector(".unidade").value=""
+
+}
+
+if (cont==5) {
+    document.querySelector(".compra5").style.display="block"
+  nomepro5.textContent=produto[valor]
+  quant5.textContent=unid
+   pre5.textContent="R$"+"   "+preco[valor]+",00"
+  v5.textContent="R$"+"   "+preco[valor]*unid+",00"
+  soma=soma+preco[valor]*unid
+  vap.textContent="Soma dos serviços/itens    R$"+"     "+soma+",00"
+  valqfalta.textContent=soma+".00"
+  select.selectedIndex=""
+  document.querySelector(".unidade").value=""
+  
+}
+
+ document.querySelector(".selec").selectedIndex=""
+ document.querySelector(".unidade").value=""         
+
+
+  
+}
+
+
+}
+}
+ 
+} 
+
+
+
+ 
 
 })
   
@@ -147,10 +217,16 @@ btn.addEventListener("click",(evt)=>{
         document.querySelector(".pesq").style.display="block"
         document.querySelector(".nome").value=""
         document.querySelector(".client").textContent=""
+        document.querySelector(".compra5").style.display="none"
+        document.querySelector(".compra4").style.display="none"
+        document.querySelector(".compra3").style.display="none"
+        document.querySelector(".compra2").style.display="none"
+        document.querySelector(".compra1").style.display="none"
+
+
         clearInterval(display)
-    }, 6000);
+    }, 3000);
  
     
      window.print()
 })
-
